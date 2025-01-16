a5dc210dcc873216563c1aeab4da953b79284f80
MunozA05 <alejandromunozf005@gmail.com>
2025-01-16 16:05:47 -0500

    Layout

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div>©2025 Hope Care. All rights reserved. </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Footer
