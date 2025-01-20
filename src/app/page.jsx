eaa1662cea200af6c73e57f632a55fccf8d36d73
MunozA05 <alejandromunozf005@gmail.com>
2025-01-20 13:39:41 -0500

    css mejorado

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,25 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
+
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          HOPE CARE Para el cuidado de tu salud.
+        </h1>
+        <p className={styles.desc}>
+        En HopeCare, nuestra misión es brindar apoyo,
+        conocimiento y soluciones para las personas que viven con enfermedades raras y sus familias.
+        </p>
+
+        <button className={styles.button}>Mira nuestro trabajo</button>
+
+      </div>
+      <div>
+        <img src="/hope.png" alt="" className={styles.img} />
+      </div>
     </div>
-  );
+  )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,12 @@ const Footer = () => {
   return (
     <div className={styles.container}>
         <div>©2025 Hope Care. All rights reserved. </div>
-        <div></div>
+        <div className={styles.social}>
+          <img src="/facebook.png" width={15} height={15} className={styles.icon} alt="Hope Dev facebook" />
+          <img src="/instagram.png" width={15} height={15} className={styles.icon} alt="Hope Dev instagram" />
+          <img src="/whatsapp.png" width={15} height={15} className={styles.icon} alt="Hope Dev whatsapp" />
+          
+        </div>
     </div>
   )
 }
