f2a504fbce09a700d6810cc3e3811e505dc76c12
MunozA05 <alejandromunozf005@gmail.com>
2025-01-21 11:58:40 -0500

    Category comienzo

--- src/app/portafolio/[Category]/page.jsx
+++ src/app/portafolio/[Category]/page.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import styles from './page.module.css'
+import Button from '@/components/Button/Button';
+import Image from 'next/image';
 
 
-const Category = () => {
+const Category = ({params}) => {
+  console.log(params);
+  
   return (
-    <div className={styles.container}>Category</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.Category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="Ver Mas" url="#"/>
+        </div>
+          <div className={styles.imgContainer}>
+            <Image
+            className={styles.img}
+            src="/medicos.png"
+            fill={true}
+            alt=""
+            />
+          </div>
+      </div>
+    </div>
+  );
+};
 
-export default Category
+export default Category;
--- src/app/portafolio/page.jsx
+++ src/app/portafolio/page.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 
 const Portafolio = () => {
   return (
-    <div className={styles.container}>Portafolio</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Nuestros Metodos</h1>
+      <div className={styles.items}>
+        <Link href="/portafolio/medicos" className={styles.item}>
+          <span className={styles.title}>Medicos</span>
+        </Link>
+        <Link href="/portafolio/clinicas" className={styles.item}>
+          <span className={styles.title}>Clinicas</span>
+        </Link>
+        <Link href="/portafolio/application" className={styles.item}>
+          <span className={styles.title}>Aplicación</span>
+        </Link>
 
-export default Portafolio
+      </div>
+    </div>
+  );
+};
+
+export default Portafolio;
