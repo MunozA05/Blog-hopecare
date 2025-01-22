398116493008d1da1e5ccd13438a919aad6ccdb2
MunozA05 <alejandromunozf005@gmail.com>
2025-01-22 13:11:00 -0500

    Category comienzo

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,11 +1,62 @@
 import React from 'react'
 import styles from './page.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
 
 const Blog = () => {
   return (
-    <div className={styles.container}>Blog</div>
-  )
-}
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testId" className={styles.container}>
+       <div className={styles.imgContainer}>
+            <Image
+            src="/medi1.png"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+            />
+       </div>  
+          <div className={styles.content}>
+            <h1 className={styles.title}>Trabajando en equipo para la esperanza</h1>
+            <p className={styles.desc}>En HopeCare, creemos que la colaboración entre médicos y especialistas es esencial para encontrar soluciones innovadoras para las enfermedades raras. Nuestro enfoque está en unir fuerzas para transformar la vida de los pacientes.</p>
+          
+          </div>
+      </Link>  
+      <Link href="/blog/testId" className={styles.container}>
+       <div className={styles.imgContainer}>
+            <Image
+            src="/medi1.png"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+            />
+       </div>  
+          <div className={styles.content}>
+            <h1 className={styles.title}>Trabajando en equipo para la esperanza</h1>
+            <p className={styles.desc}>En HopeCare, creemos que la colaboración entre médicos y especialistas es esencial para encontrar soluciones innovadoras para las enfermedades raras. Nuestro enfoque está en unir fuerzas para transformar la vida de los pacientes.</p>
+          
+          </div>
+      </Link>   
+      <Link href="/blog/testId" className={styles.container}>
+       <div className={styles.imgContainer}>
+            <Image
+            src="/medi1.png"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+            />
+       </div>  
+          <div className={styles.content}>
+            <h1 className={styles.title}>Trabajando en equipo para la esperanza</h1>
+            <p className={styles.desc}>En HopeCare, creemos que la colaboración entre médicos y especialistas es esencial para encontrar soluciones innovadoras para las enfermedades raras. Nuestro enfoque está en unir fuerzas para transformar la vida de los pacientes.</p>
+          
+          </div>
+      </Link>
+    </div>
+  );
+};
 
 export default Blog
--- src/app/portafolio/[Category]/page.jsx
+++ src/app/portafolio/[Category]/page.jsx
@@ -12,20 +12,51 @@ const Category = ({params}) => {
       <h1 className={styles.catTitle}>{params.Category}</h1>
       <div className={styles.item}>
         <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
+          <h1 className={styles.title}>Trabajando en equipo para la esperanza</h1>
+          <p className={styles.desc}>En HopeCare, creemos que la colaboración entre médicos y especialistas es esencial para encontrar soluciones innovadoras para las enfermedades raras. Nuestro enfoque está en unir fuerzas para transformar la vida de los pacientes.</p>
           <Button text="Ver Mas" url="#"/>
         </div>
           <div className={styles.imgContainer}>
             <Image
             className={styles.img}
-            src="/medicos.png"
+            src="/medi1.png"
+            fill={true}
+            alt=""
+            />
+          </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Cuidando cada detalle de tu salud</h1>
+          <p className={styles.desc}>En HopeCare, entendemos la importancia de la atención personalizada. Nos enfocamos en escuchar, comprender y brindar apoyo a cada paciente, asegurándonos de que reciban el cuidado que merecen.</p>
+          <Button text="Ver Mas" url="#"/>
+        </div>
+          <div className={styles.imgContainer}>
+            <Image
+            className={styles.img}
+            src="/medi2.png"
+            fill={true}
+            alt=""
+            />
+          </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Innovación al servicio de la salud</h1>
+          <p className={styles.desc}>HopeCare trabaja en la intersección de la ciencia y la tecnología, reuniendo a expertos para desarrollar soluciones innovadoras. Nuestra meta es liderar avances en el cuidado de enfermedades raras, mejorando la calidad de vida de nuestros pacientes.</p>
+          <Button text="Ver Mas" url="#"/>
+        </div>
+          <div className={styles.imgContainer}>
+            <Image
+            className={styles.img}
+            src="/medi3.png"
             fill={true}
             alt=""
             />
           </div>
       </div>
     </div>
+    
   );
 };
 
